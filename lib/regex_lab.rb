9f66a10f849a5f9fb6241f321f36f19e5a51ae15
Romario Gustave <romagustave10@gmail.com>
2019-11-20 05:03:37 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,13 @@
 def starts_with_a_vowel?(word) 
-  if word.match(/\A[aeiou]/i) == nil 
+  if word.match(/^[aeiouAEIOU]\w+/) == nil 
     return false 
   else 
     return true 
   end
 end
 
-def words_five_letters_long(text)
-  text.scan(/\b\w(5)\b/)
+def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/un+\w+ing/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
