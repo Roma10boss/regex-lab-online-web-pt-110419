41725eec820886769c4c510b9fe87096a0f4b242
Romario Gustave <romagustave10@gmail.com>
2019-11-20 05:11:26 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -10,8 +10,12 @@ def words_starting_with_un_and_ending_with_ing(text)
   text.scan(/un+\w+ing/)
 end
 
+def words_five_letters_long(text)
+  text.scan(/\b\w{5}\b/)
+end
+
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*\$/) == nil 
+  if (text.match(/^[A-Z]/) == nil || text.match(/\W$/) == nil)
     return false
   else
     return true 
@@ -19,9 +23,13 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\(?([0-9]{3})\)?([-]?)([0-9]{3})([-]?)([0-9]{4})/) == nil 
-    return false
-  else 
+  w = phone.match(/\d\d\d\W\d\d\d\W\d\d\d\d\b/)
+  x = phone.match(/\W\d\d\d\W\d\d\d\W\d\d\d\d\b/)
+  y = phone.match(/\d\d\d\d\d\d\d\d\d\d/)
+  z = phone.match(/\W\d\d\d\W\d\d\d\d\d\d\d/)
+  if w || x || y || z 
     return true
+  else 
+    return false
   end
 end
